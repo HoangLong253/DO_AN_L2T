c65e1a2fe8edc2244fd679db9bc0e3ac2bfc9277
HoangLong253 <0306201253@caothang.edu.vn>
2022-11-07 15:42:48 +0700

    edit main

--- do_an_l2t/lib/main.dart
+++ do_an_l2t/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+import 'screens/register.dart';
+import 'screens/login.dart';
+//import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,24 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -35,9 +37,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Center(
+        child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImg.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+                padding: EdgeInsets.all(45),
+                //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(20, 40, 20, 150),
+                        child: Text(
+                          'Marvel Conquest',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.red,
+                          ),
+                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 40, 10, 50),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                            backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                            shape: MaterialStateProperty.all(RoundedRectangleBorder())
+                        ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Login()
+                            )
+                        );
+                      },
+                      child: Text('Đăng nhập'),
+                    ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 40, 10, 50),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                            backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                            shape: MaterialStateProperty.all(RoundedRectangleBorder())
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Register()
+                              )
+                          );
+                        },
+                        child: Text('Đăng kí'),
+                      ),
+                    ),
+                  ],
+                )
+            )
+        ),
       ),
-      body: Container());
+    );
   }
 }
