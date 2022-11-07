e92e2cd9180cd60c9fa4171ea1c2218260a8bbd4
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-07 15:26:36 +0700

    edit register 2

--- do_an_l2t/lib/screens/register.dart
+++ do_an_l2t/lib/screens/register.dart
@@ -1,56 +1,32 @@
 import 'package:flutter/material.dart';
+//import 'login.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Register extends StatefulWidget {
+  const Register({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<Register> createState() => _RegisterState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage("img/a17.jpg"), fit: BoxFit.cover),
+                image: AssetImage('assets/backgroundImg.jpg'),
+                fit: BoxFit.cover),
           ),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           child: Container(
             padding: EdgeInsets.all(10),
             margin: EdgeInsets.fromLTRB(
-                20, 20, 20, MediaQuery.of(context).size.height / 3.8),
-            decoration:
-                BoxDecoration(border: Border.all(color: Colors.red, width: 1)),
+                20, 120, 20, MediaQuery.of(context).size.height / 3.8),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
                   'Đăng Ký',
@@ -64,70 +40,93 @@ class _MyHomePageState extends State<MyHomePage> {
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
+                        borderSide: BorderSide(width: 2, color: Colors.red),
                       ),
                       border: OutlineInputBorder(),
                       hintText: 'Nhập username',
-                      hintStyle: TextStyle(color: Colors.white)),
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
                 Text(
                   'Password',
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: 'Nhập password',
-                      hintStyle: TextStyle(color: Colors.white)),
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.red),
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'Nhập password',
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
                 ),
                 Text(
                   'Xác nhận Password',
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: 'Nhập lại password',
-                      hintStyle: TextStyle(color: Colors.white)),
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.red),
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'Nhập lại password',
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
                 ),
                 Text(
                   'Email',
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
+                        borderSide: BorderSide(width: 2, color: Colors.red),
                       ),
                       border: OutlineInputBorder(),
                       hintText: 'Nhập Email',
-                      hintStyle: TextStyle(color: Colors.white)),
+                      hintStyle: TextStyle(color: Colors.grey),
+                      fillColor: Colors.white,
+                    ),
+                  ),
                 ),
                 ElevatedButton(
                     style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll<Color>(
-                            Colors.red.withOpacity(0.8)),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)))),
-                    onPressed: () => {},
+                      backgroundColor: MaterialStatePropertyAll<Color>(
+                          Colors.red.withOpacity(0.8)),
+                      shape:
+                          MaterialStateProperty.all(RoundedRectangleBorder()),
+                    ),
+                    onPressed: () {
+                      /*Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Login()
+                          )
+                      );*/
+                    },
                     child: const Padding(
                         padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                         child: Text('Đăng Ký'))),
