ac85fefff79e17f25b1518a8482309d8e7980410
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-07 15:22:50 +0700

    edit login

--- do_an_l2t/lib/screens/login.dart
+++ do_an_l2t/lib/screens/login.dart
@@ -1,56 +1,46 @@
 import 'package:flutter/material.dart';
+//import 'menu.dart';
 
-void main() {
-  runApp(const MyApp());
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _LoginState extends State<Login> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.red;
+      }
+      return Colors.red;
+    }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage("img/a17.jpg"), fit: BoxFit.cover),
+                image: AssetImage('assets/backgroundImg.jpg'),
+                fit: BoxFit.cover),
           ),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           child: Container(
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10),
             margin: EdgeInsets.fromLTRB(
-                20, 20, 20, MediaQuery.of(context).size.height / 4.0),
-            decoration:
-                BoxDecoration(border: Border.all(color: Colors.red, width: 1)),
+                20, 120, 20, MediaQuery.of(context).size.height / 4.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
                   'Đăng Nhập',
@@ -61,47 +51,82 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 Text(
                   'Username',
-                  textAlign: TextAlign.left,
+                  textAlign: TextAlign.start,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  style: TextStyle(color: Colors.red),
-                  decoration: InputDecoration(
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
+                        borderSide: BorderSide(width: 2, color: Colors.red),
                       ),
                       border: OutlineInputBorder(),
                       hintText: 'Nhập username',
-                      hintStyle: TextStyle(color: Colors.white)),
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
                 Text(
                   'Password',
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(color: Colors.red),
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1, color: Colors.red),
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.red),
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'Nhập password',
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      fillColor: MaterialStateProperty.resolveWith(
+                          (states) => getColor(states)),
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Lưu mật khẩu',
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
-                      border: OutlineInputBorder(),
-                      hintText: 'Nhập password',
-                      hintStyle: TextStyle(color: Colors.white)),
+                    )
+                  ],
                 ),
                 ElevatedButton(
                     style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll<Color>(
-                            Colors.red.withOpacity(0.8)),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)))),
-                    onPressed: () => {},
+                      backgroundColor: MaterialStatePropertyAll<Color>(
+                          Colors.red.withOpacity(0.8)),
+                      shape:
+                          MaterialStateProperty.all(RoundedRectangleBorder()),
+                    ),
+                    onPressed: () {
+                      /*Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Menu()
+                              ));*/
+                    },
                     child: const Padding(
                         padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        child: Text('Đăng nhập'))),
+                        child: Text('Đăng Nhập'))),
                 Text(
-                  'Bạn chưa có tài khoản? Đăng ký ngay',
+                  'Bạn chưa có tài khoản? Đăng kí ngay',
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
