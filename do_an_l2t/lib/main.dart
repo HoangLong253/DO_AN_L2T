66d560f5753f0a4b49a9af5b8a46da5b384b3a72
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-11-11 13:13:54 +0700

    sửa file main.dart

--- do_an_l2t/lib/main.dart
+++ do_an_l2t/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
-import 'screens/register.dart';
-import 'screens/login.dart';
-//import 'package:audioplayers/audioplayers.dart';
+import 'screens/start.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,91 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/backgroundImg.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-                padding: EdgeInsets.all(45),
-                //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
-                child: Column(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(20, 40, 20, 150),
-                        child: Text(
-                          'Marvel Conquest',
-                          style: TextStyle(
-                            fontSize: 40,
-                            color: Colors.red,
-                          ),
-                        ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 40, 10, 50),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                            backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                            shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                        ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Login()
-                            )
-                        );
-                      },
-                      child: Text('Đăng nhập'),
-                    ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 40, 10, 50),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                            backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                            shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Register()
-                              )
-                          );
-                        },
-                        child: Text('Đăng kí'),
-                      ),
-                    ),
-                  ],
-                )
-            )
-        ),
-      ),
+      home: Start(),
     );
   }
 }
