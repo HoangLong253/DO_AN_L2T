539c5113bfb4df4478c59135991b33c0cb3324ef
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-18 15:37:29 +0700

    sửa mode.dart

--- do_an_l2t/lib/screens/mode.dart
+++ do_an_l2t/lib/screens/mode.dart
@@ -1,161 +1,178 @@
-import 'medmode.dart';
 import 'package:flutter/material.dart';
-import 'info.dart';
 import 'ezmode.dart';
+import 'medmode.dart';
+import 'hardmode.dart';
+import 'superhard.dart';
 
 class Mode extends StatefulWidget {
   const Mode({super.key});
 
   @override
-  State<Mode> createState() => _ModeSate();
+  State<Mode> createState() => _ModeState();
 }
 
-class _ModeSate extends State<Mode> {
+class _ModeState extends State<Mode> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Chế độ chơi'),
+        backgroundColor: Colors.red,
+      ),
       body: Center(
           child: ListView(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/backgroundImg.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                  //padding: EdgeInsets.all(10),
-                  //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
-                    child: Text(
-                      'Marvel Conquest',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.red,
-                      ),
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/backgroundImg.jpg'),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Ezmode(),
-                            ));
-                      },
-                      child: Text('Dễ'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 65, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Medmode(),
-                            ));
-                      },
-                      child: Text('Trung Bình'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 65, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {},
-                      child: Text('Khó'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 65, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {},
-                      child: Text('Siêu khó'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 45, 0, 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40, 0, 50, 40),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll<Color>(Colors.red),
-                                shape:
-                                    MaterialStateProperty.all(CircleBorder()),
-                                fixedSize:
-                                    MaterialStateProperty.all(Size(0, 60))),
-                            onPressed: () {},
-                            child: Icon(Icons.settings),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(60, 0, 40, 40),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll<Color>(Colors.red),
-                                shape:
-                                    MaterialStateProperty.all(CircleBorder()),
-                                fixedSize:
-                                    MaterialStateProperty.all(Size(0, 60))),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Info(),
-                                  ));
-                            },
-                            child: Container(
-                              child: Icon(
-                                Icons.account_circle,
-                                size: 30,
+                  height: MediaQuery.of(context).size.height ,
+                  width: MediaQuery.of(context).size.width ,
+                  child: Container(
+                    //padding: EdgeInsets.all(10),
+                    //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                  shape: MaterialStateProperty.all(RoundedRectangleBorder())
                               ),
+                              onPressed: () {
+                                setState(() {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => SuperHardMode(),
+                                      )
+                                  );
+                                });
+                              },
+                              child: Text('Siêu khó'),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                  shape: MaterialStateProperty.all(RoundedRectangleBorder())
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => Hardmode(),
+                                      )
+                                  );
+                                });
+                              },
+                              child: Text('Khó'),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                  shape: MaterialStateProperty.all(RoundedRectangleBorder())
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => Medmode(),
+                                      )
+                                  );
+                                });
+                              },
+                              child: Text('Trung bình'),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  fixedSize: MaterialStateProperty.all(Size(250 , 65)),
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                  shape: MaterialStateProperty.all(RoundedRectangleBorder())
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => Ezmode(),
+                                      )
+                                  );
+                                });
+                              },
+                              child: Text('Dễ'),
+                            ),
+                          ),
+                        ],
+                      )
                   )
-                ],
-              ))),
-        ],
-      )),
+              ),
+            ],
+          )
+      ),
     );
   }
 }
+
+/*ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                      shape: MaterialStateProperty.all(CircleBorder()),
+                                      fixedSize: MaterialStateProperty.all(Size(0, 80))
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => Info(),
+                                        )
+                                    );
+                                  },
+                                  child: Container(
+                                    alignment: Alignment.centerLeft,
+                                    child: Icon(
+                                      Icons.account_circle,
+                                      size: 50,
+                                    ),
+                                  ),
+                                ),*/
+
+/*IconButton(
+                                  style: ButtonStyle(
+                                      foregroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                                      shape: MaterialStateProperty.all(CircleBorder()),
+                                      fixedSize: MaterialStateProperty.all(Size(0, 80))
+                                  ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => Info(),
+                                          )
+                                      );
+                                    },
+                                    icon: Icon(
+                                      Icons.account_circle,
+                                      color: Colors.white,
+                                    ),
+                                  iconSize: 30,
+                                )
+                              )*/
