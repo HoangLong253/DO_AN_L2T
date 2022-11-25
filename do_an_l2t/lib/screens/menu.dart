9c237430c8f11969f982b5aae279ee48f8cbbf62
HoangLong253 <0306201253@caothang.edu.vn>
2022-11-25 11:18:17 +0700

    sửa file menu.dart

--- do_an_l2t/lib/screens/menu.dart
+++ do_an_l2t/lib/screens/menu.dart
@@ -1,6 +1,14 @@
-import 'package:do_an_l2t/screens/mode.dart';
-import 'package:do_an_l2t/screens/setting.dart';
 import 'package:flutter/material.dart';
+
+import '../components/container_with_backgroundImg.dart';
+import '../components/defaultbutton.dart';
+import '../components/logo.dart';
+import '../components/roundedbutton.dart';
+import '../screens/competitive.dart';
+import '../screens/store.dart';
+
+import 'mode.dart';
+import 'setting.dart';
 import 'info.dart';
 import 'leaderboard.dart';
 
@@ -16,162 +24,74 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/backgroundImg.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                height: MediaQuery.of(context).size.height ,
-                width: MediaQuery.of(context).size.width ,
-                child: Container(
+      body: ContainerWImg(
+        child: Center(
+            child: ListView(
+              children: [
+                Container(
                   //padding: EdgeInsets.all(10),
                   //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                          child: /*Text(
-                            'Marvel Conquest',
-                            style: TextStyle(
-                              fontSize: 40,
-                              color: Colors.red,
-                            ),
-                          ),*/
-                          Image.asset(
-                            'assets/logo.png',
+                        Logo(
                             height: 250,
-                            width: 240,
-                          )
+                            width: 240
+                        ),
+                        //padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                        DefaultButton(
+                            padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                            nextScreen: Mode(),
+                            text: 'Offline',
+                        ),
+                        //padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                        DefaultButton(
+                            padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                            nextScreen: Competitive(),
+                            text: 'Đối kháng',
+                        ),
+                        //padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                        DefaultButton(
+                            padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                            nextScreen: Leaderboard(),
+                            text: 'Leaderboard'
+                        ),
+                        //padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                        DefaultButton(
+                            padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                            nextScreen: Store(),
+                            text: 'Cửa hàng',
                         ),
                         Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Mode(),
-                                  )
-                              );
-                            },
-                            child: Text('Offline'),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                            ),
-                            onPressed: () {
-
-                            },
-                            child: Text('Đối kháng'),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Leaderboard(),
-                                  )
-                              );
-                            },
-                            child: Text('Leaderboard'),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                fixedSize: MaterialStateProperty.all(Size(250 , 65)),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder())
-                            ),
-                            onPressed: () {
-
-                            },
-                            child: Text('Cửa hàng'),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
                           child:Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(40, 0, 50, 0),
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                      shape: MaterialStateProperty.all(CircleBorder()),
-                                      fixedSize: MaterialStateProperty.all(Size(0, 60))
-                                  ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => Setting(),
-                                        )
-                                    );
-                                  },
-                                  child: Icon(Icons.settings),
-                                ),
+                              //padding: EdgeInsets.fromLTRB(40, 0, 50, 0),
+                              RoundedButton(
+                                  padding: EdgeInsets.fromLTRB(40, 0, 50, 0),
+                                  size: 60,
+                                  icon: Icons.settings,
+                                  nextScreen: Setting(),
                               ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(60, 0, 40, 0),
-                                child:ElevatedButton(
-                                  style: ButtonStyle(
-                                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                      shape: MaterialStateProperty.all(CircleBorder()),
-                                      fixedSize: MaterialStateProperty.all(Size(0, 60))
-                                  ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => Info(),
-                                        )
-                                    );
-                                  },
-                                  child: Container(
-                                    child: Icon(
-                                      Icons.account_circle,
-                                      size: 25,
-                                    ),
-                                  ),
-                                ),
-                              ),
+                              //padding: EdgeInsets.fromLTRB(60, 0, 40, 0),
+                              RoundedButton(
+                                  padding: EdgeInsets.fromLTRB(60, 0, 40, 0),
+                                  size: 60,
+                                  icon: Icons.account_circle,
+                                  nextScreen: Info(),
+                                  iconSize: 25,
+                              )
                             ],
                           ),
                         )
                       ],
                     )
                 )
-            ),
-          ],
-        )
-      ),
+              ],
+            )
+        ),
+      )
     );
   }
 }
