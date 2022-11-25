6faa5c00744b8b23eb0500da2f6fcb592f727fc0
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-25 11:55:42 +0700

    sua register.dart

--- do_an_l2t/lib/screens/register.dart
+++ do_an_l2t/lib/screens/register.dart
@@ -1,143 +1,105 @@
+import 'package:do_an_l2t/components/container_with_backgroundImg.dart';
 import 'package:flutter/material.dart';
-//import 'login.dart';
+import '../components/textfieldwtext.dart';
+import 'login.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
+
 
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/backgroundImg.jpg'),
-                fit: BoxFit.cover),
+      body: ContainerWImg(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.fromLTRB(
+              20, 80, 20, 20
           ),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.fromLTRB(
-                20, 120, 20, MediaQuery.of(context).size.height / 3.8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Đăng Ký',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red),
-                ),
-                Text(
-                  'Username',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: Colors.red),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: 'Nhập username',
-                      hintStyle: TextStyle(color: Colors.grey),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Đăng Ký',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red),
+              ),
+              TextFieldWText(
+                obscureText: false,
+                headtxt: 'Username:',
+                hinttxt: 'Nhập username',
+                bordercolor: Colors.red,
+              ),
+              TextFieldWText(
+                obscureText: true,
+                headtxt: 'Password:',
+                hinttxt: 'Nhập password',
+                bordercolor: Colors.red,
+              ),
+              TextFieldWText(
+                obscureText: true,
+                headtxt: 'Xác nhận password:',
+                hinttxt: 'Nhập lại password',
+                bordercolor: Colors.red,
+              ),
+              TextFieldWText(
+                obscureText: false,
+                headtxt: 'Email:',
+                hinttxt: 'Nhập email',
+                bordercolor: Colors.red,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Colors.red.withOpacity(0.8)),
+                    shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder()
                     ),
                   ),
-                ),
-                Text(
-                  'Password',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: TextField(
-                    obscureText: true,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 2, color: Colors.red),
-                        ),
-                        border: OutlineInputBorder(),
-                        hintText: 'Nhập password',
-                        hintStyle: TextStyle(color: Colors.grey)),
-                  ),
-                ),
-                Text(
-                  'Xác nhận Password',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: TextField(
-                    obscureText: true,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 2, color: Colors.red),
-                        ),
-                        border: OutlineInputBorder(),
-                        hintText: 'Nhập lại password',
-                        hintStyle: TextStyle(color: Colors.grey)),
-                  ),
-                ),
-                Text(
-                  'Email',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: Colors.red),
-                      ),
-                      border: OutlineInputBorder(),
-                      hintText: 'Nhập Email',
-                      hintStyle: TextStyle(color: Colors.grey),
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll<Color>(
-                          Colors.red.withOpacity(0.8)),
-                      shape:
-                          MaterialStateProperty.all(RoundedRectangleBorder()),
-                    ),
-                    onPressed: () {
-                      /*Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Login()
-                          )
-                      );*/
-                    },
-                    child: const Padding(
-                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        child: Text('Đăng Ký'))),
-                Text(
-                  'Bạn đã có tài khoản? Đăng nhập ngay',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-              ],
-            ),
-          )),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Login(),
+                        )
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Text('Đăng kí'),
+                  )
+              ),
+              /*Text(
+                'Bạn đã có tài khoản? Đăng nhập ngay',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),*/
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Login()
+                      )
+                    );
+                  },
+                  child: Text('Bạn đã có tài khoản? Đăng nhập ngay',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 15, color: Colors.white),)
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
