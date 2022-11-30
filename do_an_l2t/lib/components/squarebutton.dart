5c22a90ab2000c0cf63f3df820b4ac44f03b169d
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-11-30 22:43:37 +0700

    sửa file squarebutton.dart

---
+++ do_an_l2t/lib/components/squarebutton.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SquareButton extends StatefulWidget {
+  SquareButton(
+      {Key? key, required this.child, required this.nextScreen, this.color});
+
+  final Widget child;
+  final Widget nextScreen;
+  late Color? color;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SquareButtonState();
+  }
+}
+
+class _SquareButtonState extends State<SquareButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            fixedSize: MaterialStateProperty.all(Size(25, 50)),
+            backgroundColor: MaterialStateProperty.all(
+                (widget.color != null) ? widget.color : Colors.red),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder())),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => widget.nextScreen,
+              ));
+        },
+        child: widget.child,
+      ),
+    );
+  }
+}
