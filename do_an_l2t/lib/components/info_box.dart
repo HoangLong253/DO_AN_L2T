7ad0045fea145e4f202450dbd023b4954eb254cf
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-11-30 22:41:52 +0700

    sửa file info_box.dart

---
+++ do_an_l2t/lib/components/info_box.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class InfoBox extends StatelessWidget {
+  InfoBox({super.key, required this.text});
+
+  final String text;
+  double padding = 10;
+  double padSize = 10;
+  double height = 40;
+  double width = 250;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: EdgeInsets.all(padSize),
+      child: Container(
+        color: Colors.red,
+        child: SizedBox(
+          height: height,
+          width: width,
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
