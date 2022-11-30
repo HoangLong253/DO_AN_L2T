193b93264eacfc5b7722f3ba5493eb8b23e22e9f
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-30 22:41:51 +0700

    edit smallbutton 2

--- do_an_l2t/lib/components/smallbutton.dart
+++ do_an_l2t/lib/components/smallbutton.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SmallButton extends StatefulWidget {
-  SmallButton({Key? key, required this.nextScreen, required this.text}) : super(key: key);
+  SmallButton({Key? key, required this.nextScreen, required this.text})
+      : super(key: key);
 
   final Widget nextScreen;
   final String text;
@@ -9,34 +10,28 @@ class SmallButton extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _SmallButtonState();
   }
-
 }
 
-class _SmallButtonState extends State<SmallButton>{
+class _SmallButtonState extends State<SmallButton> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return ElevatedButton(
         style: ButtonStyle(
-          backgroundColor: MaterialStatePropertyAll<Color>(
-              Colors.red.withOpacity(0.8)),
-          shape: MaterialStateProperty.all(
-              RoundedRectangleBorder()
-          ),
+          backgroundColor:
+              MaterialStatePropertyAll<Color>(Colors.red.withOpacity(0.8)),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder()),
         ),
         onPressed: () {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => widget.nextScreen,
-              )
-          );
+                builder: (context) => widget.nextScreen,
+              ));
         },
         child: Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Text(widget.text),
-        )
-    );
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Text(widget.text),
+        ));
   }
-
 }
