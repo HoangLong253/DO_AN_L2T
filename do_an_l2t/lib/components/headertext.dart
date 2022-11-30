e816f2810d287d40cc9ee59416a842aaecd0088c
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-11-30 22:45:03 +0700

    sửa file headertext.dart

---
+++ do_an_l2t/lib/components/headertext.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HeaderText extends StatelessWidget {
+  HeaderText({Key? key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 40,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
