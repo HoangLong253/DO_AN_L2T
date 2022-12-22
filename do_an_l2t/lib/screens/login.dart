d398aa02724db5b67e92e3e0ac816ac014d0aff7
tuthanh299 <0306201299@caothang.edu.vn>
2022-12-22 23:36:10 +0700

    edit login

--- do_an_l2t/lib/screens/login.dart
+++ do_an_l2t/lib/screens/login.dart
@@ -1,113 +1,146 @@
-import 'package:do_an_l2t/screens/register.dart';
+import 'package:do_an_l2t/screens/confirmpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../components/container_with_backgroundImg.dart';
 import '../components/textfieldwtext.dart';
+import '../components/checkboxwtext.dart';
+import '../components/headertext.dart';
+import '../components/smallbutton.dart';
 
-import 'menu.dart';
+import 'register.dart';
 
 class Login extends StatefulWidget {
-  const Login({super.key});
+  Login({super.key, this.showRegister});
+  VoidCallback? showRegister;
 
   @override
   State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
-  bool isChecked = false;
+  TextEditingController txtUsername = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
 
-  Color getColor(Set<MaterialState> states) {
-    const Set<MaterialState> interactiveStates = <MaterialState>{
-      MaterialState.pressed,
-      MaterialState.hovered,
-      MaterialState.focused,
-    };
-    if (states.any(interactiveStates.contains)) {
-      return Colors.red;
+  Future signIn() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: txtUsername.text.trim(),
+        password: txtPassword.text.trim(),
+      );
+    } on FirebaseAuthException catch (e){
+      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.message.toString())));
+    } catch (e){
+      print(e);
     }
-    return Colors.red;
+  }
+
+  @override
+  void dispose() {
+    txtUsername.dispose();
+    txtPassword.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ContainerWImg(
-      child: Container(
-        padding: EdgeInsets.all(30),
-        margin: EdgeInsets.fromLTRB(
-            20, 120, 20, MediaQuery.of(context).size.height / 6.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Đăng Nhập',
-              style: TextStyle(
-                  fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
-            ),
-            TextFieldWText(
-              obscureText: false,
-              headtxt: 'Username: ',
-              hinttxt: 'Nhập username',
-              bordercolor: Colors.red,
-            ),
-            TextFieldWText(
-              obscureText: true,
-              headtxt: 'Password: ',
-              hinttxt: 'Nhập password',
-              bordercolor: Colors.red,
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  checkColor: Colors.white,
-                  fillColor: MaterialStateProperty.resolveWith(
-                      (states) => getColor(states)),
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  },
-                ),
-                Text(
-                  'Lưu mật khẩu',
-                  style: TextStyle(
-                    color: Colors.white,
+      body: ContainerWImg(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              margin: EdgeInsets.fromLTRB(20, 60, 20, MediaQuery.of(context).size.height/ 6.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  HeaderText(text: 'Đăng Nhập'),
+                  TextFieldWText(
+                    controller: txtUsername,
+                    obscureText: false,
+                    headtxt: 'Email: ',
+                    hinttxt: 'Nhập emal',
+                    bordercolor: Colors.red,
                   ),
-                )
-              ],
+                  SizedBox(height: 20,),
+                  TextFieldWText(
+                    controller: txtPassword,
+                    obscureText: true,
+                    headtxt: 'Password: ',
+                    hinttxt: 'Nhập password',
+                    bordercolor: Colors.red,
+                  ),
+                  SizedBox(height: 30,),
+                  SmallButton(
+                      onPressed: () {
+                        if(txtPassword.text == "" || txtUsername.text == "") {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Vui lòng nhập đầy đủ các trường dữ liệu')));
+                        } else {
+                          signIn();
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(builder: (context) => ConfirmPage()),
+                                  (route) => route.isFirst);
+                        }
+                      },
+                      text: 'Đăng nhập'
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => Register()),
+                                (route) => route.isFirst
+                        );
+                      },
+                      child: Text('Bạn chưa có tài khoản? Đăng kí ngay',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 15, color: Colors.white),)
+                  )
+                ],
+              ),
             ),
-            ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(
-                      Colors.red.withOpacity(0.8)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder()),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Menu(),
-                      ));
-                },
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Text('Đăng nhập'),
-                )),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Register()));
-                },
-                child: Text(
-                  'Bạn chưa có tài khoản? Đăng kí ngay',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ))
-          ],
-        ),
-      ),
-    ));
+          )
+        )
+      )
+    );
   }
 }
+
+/*Bạn chưa có tài khoản? Đăng kí ngay*/
+
+/*if (txtUsername.text.isNotEmpty || txtPassword.text.isNotEmpty) {
+                        signIn();
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => ConfirmPage()),
+                                (route) => route.isFirst);
+                      } else {
+                        print('alo');
+                      }*/
+/*CheckBoxWText(
+                    isChecked: ,
+                    text: 'Lưu mật khẩu',
+                    color: Colors.red,
+                ),*/
+/*ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Colors.red.withOpacity(0.8)),
+                    shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder()
+                    ),
+                  ),
+                  onPressed: () {
+                    signIn;
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => ConfirmPage()),
+                            (route) => route.isFirst);
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Text('Đăng nhập'),
+                  )
+              ),*/
