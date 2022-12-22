1480c73a6ad94c2029fdcf848bf755d14471980c
HoangLong253 <0306201253@caothang.edu.vn>
2022-12-23 00:16:05 +0700

    sửa file mode.dart

--- do_an_l2t/lib/screens/mode.dart
+++ do_an_l2t/lib/screens/mode.dart
@@ -1,10 +1,10 @@
-//import 'package:do_an_mon/screens/medmode.dart';
+import 'package:do_an_l2t/components/container_with_backgroundImg.dart';
+import 'package:do_an_l2t/components/defaultbutton.dart';
 import 'package:flutter/material.dart';
-
 import 'ezmode.dart';
 import 'medmode.dart';
-//import 'info.dart';
-//import 'ezmode.dart';
+import 'hardmode.dart';
+import 'superhard.dart';
 
 class Mode extends StatefulWidget {
   const Mode({super.key});
@@ -14,6 +14,7 @@ class Mode extends StatefulWidget {
 }
 
 class _ModeState extends State<Mode> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,199 +22,57 @@ class _ModeState extends State<Mode> {
         title: Text('Chế độ chơi'),
         backgroundColor: Colors.red,
       ),
-      body: Center(
-          child: ListView(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/backgroundImg.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                  //padding: EdgeInsets.all(10),
-                  //margin: EdgeInsets.fromLTRB(20, 20, 20, MediaQuery.of(context).size.height),
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        /*Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Ezmode(),
-                            ));*/
-                      },
-                      child: Text('Siêu khó'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        /*Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Medmode(),
-                            ));*/
-                      },
-                      child: Text('Khó'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Medmode(),
-                              ));
-                        });
-                      },
-                      child: Text('Trung bình'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(Size(250, 65)),
-                          backgroundColor:
-                              MaterialStatePropertyAll<Color>(Colors.red),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder())),
-                      onPressed: () {
-                        setState(() {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Ezmode(),
-                              ));
-                        });
-                      },
-                      child: Text('Dễ'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 45, 0, 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40, 0, 50, 40),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll<Color>(Colors.red),
-                                shape:
-                                    MaterialStateProperty.all(CircleBorder()),
-                                fixedSize:
-                                    MaterialStateProperty.all(Size(0, 60))),
-                            onPressed: () {},
-                            child: Icon(Icons.settings),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(60, 0, 40, 40),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll<Color>(Colors.red),
-                                shape:
-                                    MaterialStateProperty.all(CircleBorder()),
-                                fixedSize:
-                                    MaterialStateProperty.all(Size(0, 60))),
-                            onPressed: () {
-                              /*Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Info(),
-                                  ));*/
-                            },
-                            child: Container(
-                              child: Icon(
-                                Icons.account_circle,
-                                size: 30,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ))),
-        ],
-      )),
+      body: ContainerWImg(
+        child: Column(
+          verticalDirection: VerticalDirection.down,
+          children: [
+            DefaultButton(
+              padding: EdgeInsets.fromLTRB(20, 120, 10, 0),
+              text: 'Siêu khó',
+              nextScreen: SuperHardMode(),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SuperHardMode())
+                );
+              },
+            ),
+            DefaultButton(
+              padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+              text: 'Khó',
+              nextScreen: Hardmode(),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Hardmode())
+                );
+              },
+            ),
+            DefaultButton(
+              padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+              text: 'Trung bình',
+              nextScreen: Medmode(),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Medmode())
+                );
+              },
+            ),
+            DefaultButton(
+              padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+              text: 'Dễ',
+              nextScreen: Ezmode(),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Ezmode())
+                );
+              },
+            ),
+          ],
+        )
+      )
     );
   }
 }
-
-/*ElevatedButton(
-                                  style: ButtonStyle(
-                                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                      shape: MaterialStateProperty.all(CircleBorder()),
-                                      fixedSize: MaterialStateProperty.all(Size(0, 80))
-                                  ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => Info(),
-                                        )
-                                    );
-                                  },
-                                  child: Container(
-                                    alignment: Alignment.centerLeft,
-                                    child: Icon(
-                                      Icons.account_circle,
-                                      size: 50,
-                                    ),
-                                  ),
-                                ),*/
-
-/*IconButton(
-                                  style: ButtonStyle(
-                                      foregroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                                      shape: MaterialStateProperty.all(CircleBorder()),
-                                      fixedSize: MaterialStateProperty.all(Size(0, 80))
-                                  ),
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => Info(),
-                                          )
-                                      );
-                                    },
-                                    icon: Icon(
-                                      Icons.account_circle,
-                                      color: Colors.white,
-                                    ),
-                                  iconSize: 30,
-                                )
-                              )*/
