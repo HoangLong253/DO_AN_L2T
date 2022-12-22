ee3520d9e9eac3bdd629ad0a79cf9f44284f2b95
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-12-22 23:52:38 +0700

    sửa file start.dart

--- do_an_l2t/lib/screens/start.dart
+++ do_an_l2t/lib/screens/start.dart
@@ -1,10 +1,10 @@
-import 'package:do_an_l2t/components/logo.dart';
 import 'package:flutter/material.dart';
-
 import '../components/container_with_backgroundImg.dart';
 import '../components/defaultbutton.dart';
-import 'login.dart';
+import '../components/logo.dart';
+
 import 'register.dart';
+import 'confirmpage.dart';
 
 class Start extends StatefulWidget {
   Start({super.key});
@@ -20,21 +20,31 @@ class _StartState extends State<Start> {
       body: Center(
           child: ContainerWImg(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Logo(
               height: 250,
               width: 240,
             ),
             DefaultButton(
-              padding: EdgeInsets.fromLTRB(20, 5, 10, 50),
+              padding: EdgeInsets.fromLTRB(20, 30, 10, 50),
               text: 'Đăng nhập',
-              nextScreen: Login(),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ConfirmPage()));
+              },
               color: Colors.red,
             ),
             DefaultButton(
               padding: EdgeInsets.fromLTRB(20, 5, 10, 50),
               text: 'Đăng kí',
-              nextScreen: Register(),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Register(showLogin: () {}),
+                    ));
+              },
               color: Colors.red,
             ),
           ],
