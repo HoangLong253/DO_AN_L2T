eecdd13bcf8c9ce8f6b8ce64d8142e7be33f5637
Tuan298 <113426773+Tuan298@users.noreply.github.com>
2022-12-22 23:53:27 +0700

    sửa file superhard.dart

--- do_an_l2t/lib/screens/superhard.dart
+++ do_an_l2t/lib/screens/superhard.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import '../components/container_with_backgroundImg.dart';
 import '../components/headertext.dart';
 import '../components/squarebutton.dart';
+import 'complete.dart';
 
 class SuperHardMode extends StatefulWidget {
   const SuperHardMode({super.key});
@@ -35,23 +36,23 @@ class _SuperHardModeState extends State<SuperHardMode> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('1'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('2'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('3'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('4'),
-                        nextScreen: Question(),
-                      ),
-                      SquareButton(
-                        child: Text('5'),
                         nextScreen: Question(),
                       ),
                     ],
@@ -60,28 +61,43 @@ class _SuperHardModeState extends State<SuperHardMode> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
+                        child: Text('5'),
+                        nextScreen: Question(),
+                      ),
+                      SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('6'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('7'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('8'),
                         nextScreen: Question(),
                       ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
                       SquareButton(
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
                         child: Text('9'),
                         nextScreen: Question(),
                       ),
                       SquareButton(
-                        nextScreen: Question(),
+                        padding: EdgeInsets.fromLTRB(20, 60, 10, 0),
+                        nextScreen: Complete(),
                         child: Text('10'),
                         color: Colors.purple,
                       )
                     ],
-                  ),
+                  )
                 ],
               )),
             ],
