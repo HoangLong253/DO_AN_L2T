b939e045dea08cc193d4a9fc09476a2d1fbc17c7
HoangLong253 <0306201253@caothang.edu.vn>
2022-12-24 11:57:04 +0700

    sửa main.dart

--- do_an_l2t/lib/main.dart
+++ do_an_l2t/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/start.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Marvel Conquest',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
